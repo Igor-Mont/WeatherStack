d95fd2edcc5efcba66846e0d3a3d1795828938b8
Igor Monteiro <igornathandev@gmail.com>
2021-08-02 15:21:29 -0300

    Changes in useState

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,21 @@ type DataCityProps = {
   name: string;
 };
 
+const initialState = {
+  weather: [{ description: 'none' }],
+  main: {
+    temp: 0,
+    feels_like: 0,
+    temp_min: 0,
+    temp_max: 0,
+    humidity: 0,
+  },
+  wind: {
+    speed: 0,
+  },
+  name: 'None',
+};
+
 function App(): JSX.Element {
   const [dataCity, setDataCity] = useState<DataCityProps>();
   const [inputSearchCity, setInputSearchCity] = useState('');
@@ -54,48 +69,52 @@ function App(): JSX.Element {
     <MainGrid>
       <GlobalStyle />
       <div className="container">
-        <div className="search">
-          <label htmlFor="inputSearch">
-            <img src={searchImg} alt="Search" />
-            <input
-              type="text"
-              id="inputSearch"
-              placeholder="Pesquise sua cidade"
-              value={inputSearchCity}
-              onChange={e => setInputSearchCity(e.target.value)}
-              onKeyDown={e => e.keyCode === 13 && handleSearchCity()}
-              tabIndex={0}
-            />
-          </label>
-          <button type="submit" onClick={handleSearchCity}>
-            Buscar
-          </button>
-        </div>
-        <div className="content">
-          <div className="left-content">
-            <h1>{dataCity?.name}</h1>
-            <div className="img-weather">
-              <img src={cloudySvg} alt="Cloudy" />
+        {dataCity && (
+          <>
+            <div className="search">
+              <label htmlFor="inputSearch">
+                <img src={searchImg} alt="Search" />
+                <input
+                  type="text"
+                  id="inputSearch"
+                  placeholder="Pesquise sua cidade"
+                  value={inputSearchCity}
+                  onChange={e => setInputSearchCity(e.target.value)}
+                  onKeyDown={e => e.keyCode === 13 && handleSearchCity()}
+                  tabIndex={0}
+                />
+              </label>
+              <button type="submit" onClick={handleSearchCity}>
+                Buscar
+              </button>
             </div>
-            <div className="more-infos">
-              <p>Ventos: {dataCity?.wind.speed}</p>
-              <p>Umidade: {dataCity?.main.humidity}</p>
-            </div>
-          </div>
-          <div className="right-content">
-            <div className="infos">
-              <h2>{Math.round(temp)} ºC</h2>
-              <p>Sensação térmica: {Math.floor(thermalSensation)} ºC</p>
-              <div className="min-max">
-                <p>Min: {Math.floor(tempMin)} ºC</p>
-                <p>Max: {Math.floor(tempMax)} ºC</p>
+            <div className="content">
+              <div className="left-content">
+                <h1>{dataCity?.name}</h1>
+                <div className="img-weather">
+                  <img src={cloudySvg} alt="Cloudy" />
+                </div>
+                <div className="more-infos">
+                  <p>Ventos: {dataCity?.wind.speed}</p>
+                  <p>Umidade: {dataCity?.main.humidity}</p>
+                </div>
+              </div>
+              <div className="right-content">
+                <div className="infos">
+                  <h2>{Math.round(temp)} ºC</h2>
+                  <p>Sensação térmica: {Math.floor(thermalSensation)} ºC</p>
+                  <div className="min-max">
+                    <p>Min: {Math.floor(tempMin)} ºC</p>
+                    <p>Max: {Math.floor(tempMax)} ºC</p>
+                  </div>
+                </div>
+                <div className="weather">
+                  <h2>{dataCity?.weather[0].description}</h2>
+                </div>
               </div>
             </div>
-            <div className="weather">
-              <h2>{dataCity?.weather[0].description}</h2>
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </MainGrid>
   );
